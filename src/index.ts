e8605ee6be46eb1afc8406b281b485983de48e72
pmardaniyan <p.mardaniyan@gmail.com>
2022-11-13 10:27:46 +0330

    Database initialize

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log('hello world');
+import { DataSource } from "typeorm"
+
+
+import { config } from "dotenv"
+
+
+config()
+
+
+const AppDataSource = new DataSource ({
+    type: "postgres",
+    host: "localhost",
+    port: process.env.PORT as unknown as number,
+    username: "postgres",
+    password: process.env.PASSWORD_DB as unknown as string,
+    database: "orm",
+    entities: [
+        // ....
+    ],
+
+
+})
+
+
+const main =async () => {
+    
+    await AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
+}
+
+
+main()
